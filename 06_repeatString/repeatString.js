1a6f4050f3eacf90dd7b89bbab48b064eda6493e
Tushar Rout <tusharrout01@gmail.com>
2025-11-16 17:26:06 +0000

    Make the first test task pass

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(passedString, repeated) {
 
+    let sum="";
+   for(let i=1;i<=repeated;i++)
+   {
+    sum += passedString;
+   }
+   return sum;
 };
+
+//repeatString("Uday",3);
 
 // Do not edit below this line
 module.exports = repeatString;
