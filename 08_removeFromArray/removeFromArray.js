4c46d49468421a2f5ea0cc5436fa40fec4a7b144
Tushar Rout <tusharrout01@gmail.com>
2025-11-16 19:03:32 +0000

    complete according to TOPs description

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(passedArray, ...extraArguments) {
+  return passedArray.filter(item => !extraArguments.includes(item));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
