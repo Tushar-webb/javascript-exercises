34deda668510b6676985a9e2559a414604fa4072
Tushar Rout <tusharrout01@gmail.com>
2025-11-17 08:59:20 +0000

    complete according to TOPs description

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (startingNum, endingNum) {
+    if (Number.isInteger(startingNum) && Number.isInteger(endingNum) && startingNum > -1 && endingNum > -1) {
+        let sum = 0;
+        if (startingNum < endingNum) {
+            for (let i = startingNum; i <= endingNum; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        else {
+            for (let i = startingNum; i >= endingNum; i--) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
